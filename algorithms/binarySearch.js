d6d885fc42060eb1f441454e895d5bc354842bc4
lanxiaojing <547418453@qq.com>
2018-04-19 23:05:22 +0800

    feat: add file

---
+++ algorithms/binarySearch.js
@@ -0,0 +1,17 @@
+function rank(target, arr) {
+    let start = 0
+    let end = arr.length - 1
+    while(start < end) {
+        let mid = Math.ceil(start + (end - start) / 2)
+        if(arr[mid] === target) {
+            return mid
+        }
+        if(arr[mid] > target) {
+            end = mid - 1
+        }
+        if(arr[mid] < target) {
+            start = mid + 1
+        }
+    }
+    return -1
+}
